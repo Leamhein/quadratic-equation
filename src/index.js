23e07e302d139ff06d08c884c523b42d772ebec6
Vladimir Apenko <Leamhein@hotmail.com>
2018-02-20 23:22:39 +0300

    Make an array of a,b,c from square equation and add an discriminant

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var eq = equation.split("^2").join("").split(" * x "); //split the equation into parts without "x" and "^2"
+  for (let i = 0; i < eq.length; i++) {
+    eq[i] = eq[i].split("");
+    for (let g = 0; g < eq[i].length; g++) {
+      if (eq[i][g] == " ") { //delete gaps in array
+        eq[i].splice(g, 1);
+      }
+    }
+    eq[i] = eq[i].join("");
+  } //after the above actions, we have an array with values a, b, c from the quadratic equation
+  var D = +eq[1] * +eq[1] - 4 * +eq[0] * +eq[2]; //discriminant
 }
